2991d82901ccc0b70a392a16ac75ca13c4326534
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2022-04-20 16:09:01 +0000

    Refactoring code, using type hints, Black formatting, etc.

--- intezer_static_client.py
+++ intezer_static_client.py
@@ -1,33 +1,39 @@
-import requests
+from requests import post, session
+from typing import Dict
 
-class IntezerStaticClient():
-	def __init__(self, apikey=""):
-		self.apikey = apikey
-		self.headers = { "api_key": self.apikey }
-		self.base_url = "https://analyze.intezer.com/api/v2-0"
-		self.create_session()
 
-	def create_session(self):
-		response = requests.post(self.base_url + "/get-access-token", json=self.headers)
-		self.session = requests.session()
-		self.session.headers["Authorization"] = self.session.headers["Authorization"] = "Bearer %s" % response.json()["result"]
+class IntezerStaticClient:
+    def __init__(self, apikey: str = "") -> None:
+        self.apikey: str = apikey
+        self.headers: Dict[str, str] = {"api_key": self.apikey}
+        self.base_url: str = "https://analyze.intezer.com/api/v2-0"
+        self.create_session()
 
-	def get_hash_results(self, sha256):
-		response = self.session.get(self.base_url + "/files/" + sha256)
-		
-		if response.status_code == 200:
-			return response.json()["result"]
+    def create_session(self) -> None:
+        response = post(f"{self.base_url}/get-access-token", json=self.headers)
+        self.session = session()
+        self.session.headers["Authorization"] = f"Bearer {response.json()['result']}"
 
-	def get_sub_analysis(self, analysis_id):
-		response = self.session.get(self.base_url + "/analyses/" + analysis_id + "/sub-analyses")
+    def get_hash_results(self, sha256: str) -> None:
+        response = self.session.get(f"{self.base_url}/files/{sha256}")
 
-		return response.json()
+        if response.status_code == 200:
+            return response.json()["result"]
 
-	def get_code_reuse(self, analysis_id, sub_analysis_id):
-		response = self.session.get(self.base_url + "/analyses/" + analysis_id + "/sub-analyses/" + sub_analysis_id + "/code-reuse")
-		return response.json()
+    def get_sub_analysis(self, analysis_id: str) -> None:
+        response = self.session.get(
+            f"{self.base_url}/analyses/{analysis_id}/sub-analyses"
+        )
+        return response.json()
 
-	def get_metadata(self, analysis_id, sub_analysis_id):
-		response = self.session.get(self.base_url + "/analyses/" + analysis_id + "/sub-analyses/" + sub_analysis_id + "/metadata")
+    def get_code_reuse(self, analysis_id: str, sub_analysis_id: str) -> None:
+        response = self.session.get(
+            f"{self.base_url}/analyses/{analysis_id}/sub-analyses/{sub_analysis_id}/code-reuse"
+        )
+        return response.json()
 
-		return response.json()
+    def get_metadata(self, analysis_id: str, sub_analysis_id: str) -> None:
+        response = self.session.get(
+            f"{self.base_url}/analyses/{analysis_id}/sub-analyses/{sub_analysis_id}/metadata"
+        )
+        return response.json()
